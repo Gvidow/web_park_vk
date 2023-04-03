8e09c4f3ecebdcf34a9609f8589a953266869da5
Gvidow <arzamachev.art-alex@yandex.ru>
2023-04-03 22:26:40 +0300

    add page answers on questions

--- askme/app/models.py
+++ askme/app/models.py
@@ -16,15 +16,29 @@ class Question:
         self.title = title
         self.text = text
 
+class Answer:
+    def __init__(self, id, author, question, text, corect, count_like):
+        self.id = id
+        self.author = author
+        self.question = question
+        self.text = text
+        self.corect = corect
+        self.count_like = count_like
+
 
 def get_questions():
     return [{
         "id": q.id,
         "title": q.title,
         "text": q.text,
-        "avatar": "img/" + q.author.avatar,
+        "avatar": q.author.avatar,
     } for q in QUESTIONS
     ]
+
+def get_question(id):
+    for q in QUESTIONS:
+        if q.id == id:
+            return q
 
 
 def get_user(ok):
@@ -32,14 +46,25 @@ def get_user(ok):
         return None
     else:
         return USERS[0]
+    
+def get_responses(question_id):
+    res = []
+    for a in ANSWERS:
+        if a.question.id == question_id:
+            res.append(a)
+    return res
+
 
 
 COUNT_USERS = 6
 COUNT_QUESTION = 10
+COUNT_ANSWER = 30
 
-PICTURES = ["user1.png", "user2.jpeg", "user3.png"]
+PICTURES = ["img/user1.png", "img/user2.jpeg", "img/user3.png"]
 
 USERS = [User(i, choice(PICTURES)) for i in range(COUNT_USERS)]
 
 QUESTIONS = [Question(i, choice(USERS), f"Title {i}", f"Text {i}") for i in range(COUNT_QUESTION)]
+
+ANSWERS = [Answer(i, choice(USERS), choice(QUESTIONS), f"{i} I think everything will be fine", choice([True, False]), 12 * i) for i in range(COUNT_ANSWER)]
 
--- askme/app/views.py
+++ askme/app/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
-from .models import get_questions, get_user
+from .models import get_questions, get_user, get_question, get_responses
 from random import choice
 
 
 def index(request):
     is_authorized = choice([True, False])
-    context = {"questions": get_questions, "user_data": get_user(is_authorized)}
+    context = {"questions": get_questions(), "user_data": get_user(is_authorized)}
     return render(request, "index.html", context)
 
 
 def question(request, id):
     is_authorized = choice([True, False])
     context = {
-        "question_id": id,
+        "question": get_question(id),
         "user_data": get_user(is_authorized),
+        "responses": get_responses(id),
     }
     return render(request, "question.html", context)
